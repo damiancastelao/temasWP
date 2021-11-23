e1fb41443784007a8858275a0f59ae5e792cb905
Damian Nogueiras <dmian@danielcastelao.org>
2021-11-23 09:41:46 +0100

    404 terminada

---
+++ html/wp-content/themes/newFuncy/404.php
@@ -0,0 +1,17 @@
+<?php
+get_header();
+?>
+
+<!-- Aqui va mi 404 -->
+
+<div id="contenedor">
+    <div style="background-color: green" id="cabecera" class="rojo">
+        <h1>Hola</h1>
+    </div>
+    <div id="cuerpo">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/images/totoro.jpeg">
+    </div>
+</div>
+
+<?php
+get_footer();
